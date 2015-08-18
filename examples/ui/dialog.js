4d6c25059204cefcf7c4bcbab7639914dea7b72e
张云来 <cloudcome@163.com>
2015-08-18 16:38:42 +0800

    ui/dialog/

---
+++ examples/ui/dialog.js
@@ -0,0 +1,18 @@
+/*!
+ * 文件描述
+ * @author ydr.me
+ * @create 2015-08-18 16:28
+ */
+
+
+define(function (require, exports, module) {
+    /**
+     * @module parent/dialog.js
+     */
+
+    'use strict';
+
+    var abc = require('./abc.js');
+
+    module.exports = {};
+});
