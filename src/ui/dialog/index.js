f7d2de0bbd75f3232beeb32c264963454bfcafb0
张云来 <cloudcome@163.com>
2015-08-18 12:07:50 +0800

    ui/dialog/

---
+++ src/ui/dialog/index.js
@@ -0,0 +1,17 @@
+/*!
+ * 文件描述
+ * @author ydr.me
+ * @create 2015-08-18 12:07
+ */
+
+
+define(function (require, exports, module) {
+    /**
+     * @module parent/index
+     */
+
+    'use strict';
+
+
+    module.exports = {};
+});
