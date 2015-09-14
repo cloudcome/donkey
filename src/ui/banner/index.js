f1345c2ff43c0764a1c1cf88d25e74186f2cc8ee
张云来 <cloudcome@163.com>
2015-09-14 23:11:11 +0800

    banner

---
+++ src/ui/banner/index.js
@@ -0,0 +1,22 @@
+/*!
+ * 文件描述
+ * @author ydr.me
+ * @create 2015-09-14 22:43
+ */
+
+
+define(function (require, exports, module) {
+    /**
+     * @module parent/index
+     */
+
+    'use strict';
+
+    var $ = window.jQuery;
+    var ui = require('../index.js');
+    var Banner = ui.create({
+
+    });
+
+    module.exports = {};
+});
