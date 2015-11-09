36093f0e4a1287cda02827f05e3798cb6f9db55f
cloudcome <cloudcome@163.com>
2015-11-09 13:03:31 +0800

    coolie avalon

---
+++ examples/avalon/1.js
@@ -0,0 +1,18 @@
+/**
+ * 文件描述
+ * @author ydr.me
+ * @create 2015-11-09 13:01
+ */
+
+
+define(function (require, exports, module) {
+    /**
+     * @module parent/1.js
+     */
+
+    'use strict';
+
+    var abc = require('./abc.js');
+
+    module.exports = {};
+});
