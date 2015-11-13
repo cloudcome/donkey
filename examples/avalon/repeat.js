ecb6afb5efebd6f038fc9db255eb334a5e311ed8
cloudcome <cloudcome@163.com>
2015-11-13 11:15:38 +0800

    mvvm

--- examples/avalon/repeat.js
+++ examples/avalon/repeat.js
@@ -12,24 +12,32 @@ define(function (require, exports, module) {
 
     'use strict';
 
-    var avalon = window.avalon;
-    var vm = avalon.define({
-        $id: 'repeat',
-        list1: ['abc', 'def', 'xyz'],
-        list2: {
-            abc: 'ABC',
-            def: 'DEF',
-            xyz: 'XYZ'
+    var Avalon = window.Avalon;
+    Avalon.filter('add', function (val, val2) {
+        return val + val2;
+    });
+    var vm = new Avalon({
+        el: 'repeat',
+        data: {
+            list1: ['<h1>abc</h1>', 'def', 'xyz'],
+            list2: {
+                abc: 'ABC',
+                def: 'DEF',
+                xyz: 'XYZ'
+            }
         },
-        addSuffix: function (index) {
-            vm.list1.set(index, vm.list1[index] + '0');
-        },
-        remove: function (index) {
-            vm.list1.splice(index, 1);
-        },
-        push: function () {
-            vm.list1.push(Date.now());
+        methods: {
+            addSuffix: function (index) {
+                vm.list1.set(index, vm.list1[index] + '0');
+            },
+            remove: function (index) {
+                vm.list1.splice(index, 1);
+            },
+            push: function () {
+                vm.list1.push(Date.now());
+            }
         }
     });
-    avalon.scan();
+
+    window.vm = vm;
 });
