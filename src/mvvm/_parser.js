628a4d0afe84679d9156a3519895ac7713bac59c
cloudcome <cloudcome@163.com>
2015-12-04 20:53:42 +0800

    mvvm

--- src/mvvm/_parser.js
+++ src/mvvm/_parser.js
@@ -8,26 +8,67 @@
 define(function (require, exports, module) {
     'use strict';
 
+    var dato = require('../utils/dato.js');
+
+    var operators = [' ', '+', '-', '*', '/', '>', '<', '=', '!']
+    var operatorsMap = {};
+
+    dato.each(operators, function (index, token) {
+        operatorsMap[token] = true;
+    });
+
 
     /**
-     * 分隔表达式为键值对
+     * 此法分析
      * @param expression
-     * @param keyIsVarible
      *
      * @example
-     * 'abc' + def: true
-     * // => key: 'abc' + def
-     * // => val: true
      */
-    exports.split = function (expression, keyIsVarible) {
+    module.exports = function (expression) {
         var inDoubleQuote = false;
         var inSingleQuote = false;
-
-        var arr = expression.split(':');
-
-        return {
-            key: (arr[0] || '').trim(),
-            val: (arr[1] || '').trim()
+        var inSquareBrackets = false;
+        var inPoint = false;
+        var index = 0;
+        var length = expression.length;
+        var ret = {
+            raw: expression,
+            vars: []
         };
+        var lastVar = '';
+        var lastOperator = '';
+
+        for (; index < length; index++) {
+            var char = expression[index];
+
+            if (inDoubleQuote) {
+                if (char === '"') {
+                    inDoubleQuote = false;
+                }
+            } else if (inSingleQuote) {
+                if (char === '\'') {
+                    inSingleQuote = false;
+                }
+            } else if (char === '"') {
+                inDoubleQuote = true;
+            } else if (char === '\'') {
+                inSingleQuote = true;
+            } else {
+                if (operatorsMap[char]) {
+                    if (lastVar) {
+                        ret.vars.push(lastVar);
+                        lastVar = '';
+                    }
+                } else {
+                    lastVar += char;
+                }
+            }
+        }
+
+        if (lastVar) {
+            ret.vars.push(lastVar);
+        }
+
+        return ret;
     };
 });
