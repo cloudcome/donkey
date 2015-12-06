ffbfe53887afe18fb0fd99daa5a5033ecaaa4c08
cloudcome <cloudcome@163.com>
2015-12-06 13:39:29 +0800

    mvvm

---
+++ examples/mvvm/_parser/path.js
@@ -0,0 +1,22 @@
+/**
+ * 文件描述
+ * @author ydr.me
+ * @create 2015-12-04 11:38
+ */
+
+
+define(function (require, exports, module) {
+    /**
+     * @module parent/observe
+     */
+
+    'use strict';
+
+    var _parser = require('../../../src/mvvm/_parser/path.js');
+
+    document.getElementById('parse').onclick = function () {
+        var ret = _parser(document.getElementById('textarea').value, '{{', '}}');
+
+        document.getElementById('ret').innerHTML = JSON.stringify(ret, null, 4);
+    };
+});
---
+++ src/mvvm/_parser/path.js
@@ -0,0 +1,14 @@
+/**
+ * 路径解析
+ * @author ydr.me
+ * @create 2015-12-06 13:37
+ */
+
+
+define(function (require, exports, module) {
+    'use strict';
+
+    module.exports = function (path, parentData) {
+
+    };
+});
