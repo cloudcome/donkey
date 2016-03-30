ef3f90e8a7db194d29cd944256f23055b13e7d2d
cloudcome <cloudcome@163.com>
2016-03-30 11:09:21 +0800

    控制尺寸可变元素的 style

--- src/3rd/tinymce/classes/dom/ControlSelection.js
+++ src/3rd/tinymce/classes/dom/ControlSelection.js
@@ -194,12 +194,18 @@ define(function (require, exports, module) {
 
             function setSizeProp(name, value) {
                 if (value) {
-                    // Resize by using style or attribute
-                    if (selectedElm.style[name] || !editor.schema.isValid(selectedElm.nodeName.toLowerCase(), name)) {
-                        dom.setStyle(selectedElm, name, value);
-                    } else {
+                    dom.setStyle(selectedElm, name, value);
+
+                    if (editor.schema.isValid(selectedElm.nodeName.toLowerCase(), name)) {
                         dom.setAttrib(selectedElm, name, value);
                     }
+
+                    //// Resize by using style or attribute
+                    //if (selectedElm.style[name] || !editor.schema.isValid(selectedElm.nodeName.toLowerCase(), name)) {
+                    //    dom.setStyle(selectedElm, name, value);
+                    //} else {
+                    //    dom.setAttrib(selectedElm, name, value);
+                    //}
                 }
             }
 
